a810e6ee96217c50ee9c7a484950c383a4781383
mahata <mahata777@gmail.com>
2016-09-01 00:16:26 +0900

    Refactor a bit for Anagram checker

--- books/sekai-programming/java/src/org/mahata/array_string/Anagram.java
+++ books/sekai-programming/java/src/org/mahata/array_string/Anagram.java
@@ -6,22 +6,14 @@ package org.mahata.array_string;
 public class Anagram {
     static private boolean isAnagram(String s1, String s2) {
         char[] arr1 = s1.toCharArray();
-        java.util.Arrays.sort(arr1);
-
         char[] arr2 = s2.toCharArray();
-        java.util.Arrays.sort(arr2);
-
         if (arr1.length != arr2.length) {
             return false;
         }
 
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
-
-        return true;
+        java.util.Arrays.sort(arr1);
+        java.util.Arrays.sort(arr2);
+        return java.util.Arrays.equals(arr1, arr2);
     }
 
     public static void main(String[] args) {
