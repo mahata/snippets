91c5690ec2e53a4891a73f00dde8ea8874490844
mahata <mahata777@gmail.com>
2016-08-31 23:20:14 +0900

    Add one more naive implementation

---
+++ books/sekai-programming/java/src/org/mahata/array_string/Unique2.java
@@ -0,0 +1,27 @@
+package org.mahata.array_string;
+
+/**
+ * Created by mahata on 8/31/16.
+ */
+public class Unique2 {
+    static private boolean isUnique(String s) {
+        if (s.length() > 256) {
+            return false;
+        }
+
+        int checker = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) - 'a';
+            if ((checker & (1 << val)) > 0) {
+                return false;
+            }
+            checker |= (1 << val);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUnique("Hello"));
+        System.out.println(isUnique("HHelo"));
+    }
+}
